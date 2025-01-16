37105a4ecde159c7eee49c8eee9835b81170692f
Angelisse Johnson <angelissejoh2019@fau.edu>
2025-01-16 13:32:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
-st.title("My Updated Streamlit App")
+import streamlit as st
+
+st.title("My Streamlit App")
+st.write("Welcome to my first Streamlit app deployed on Streamlit Cloud!")st.title("My Updated Streamlit App")
 st.write("This is an updated version of my Streamlit app.")
